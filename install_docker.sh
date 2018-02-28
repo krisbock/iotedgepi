aeb5997a6cb3082977b1222565ea9b068d3cdcc2
Kris Bock <krisbock@users.noreply.github.com>
2018-02-28 14:59:14 +1000

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=armhf] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
